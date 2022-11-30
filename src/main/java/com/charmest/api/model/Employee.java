626fc5c33ebbae739c8b473d30bf8bc28996e031
Thomas CHARMES <charmes.thomas@gmail.com>
2022-11-30 15:51:51 +0100

    add crud methods

--- src/main/java/com/charmest/api/model/Employee.java
+++ src/main/java/com/charmest/api/model/Employee.java
@@ -23,7 +23,47 @@ public class Employee {
     @Column(name="first_name")
     private String firstName;
 
-    // Lien avec un champ de la table
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// Lien avec un champ de la table
     @Column(name="last_name")
     private String lastName;
 
